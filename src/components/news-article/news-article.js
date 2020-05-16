3ad050785047a523f98d1cfafee29ca08ea52b89
MajaSveljo <majasveljo@gmail.com>
2020-05-16 22:04:13 +0200

    Added link css, formatting and routing to article website.

--- src/components/news-article/news-article.js
+++ src/components/news-article/news-article.js
@@ -11,29 +11,42 @@ const NewsArticle = ({
   user,
   time,
   commentsNumber,
-}) => (
-  <div className={"news-article" + " " + className}>
-    <span className="news-article__order-number">
-      {orderNumber ? orderNumber : "0"}.
-    </span>
+}) => {
+  const formatUrl = (url) => {
+    let formattedUrl = url.split("/")[2];
 
-    <div className="news-article__data-container">
-      <span className="news-article__data-container--header">
-        {header ? header : "Placeholder text for article header"}
-      </span>
-      <span className="news-article__data-container--link">
-        {website ? website : "wwebsite"}
+    if (formattedUrl.indexOf("www") === 0) {
+      formattedUrl = formattedUrl.substr(4, formattedUrl.length);
+    }
+    return formattedUrl;
+  };
+
+  return (
+    <div className={"news-article" + " " + className}>
+      <span className="news-article__order-number">
+        {orderNumber ? orderNumber : "0"}.
       </span>
 
-      <div className="news-article__data-container--properties">
-        {points ? points : "00"} points by {user ? user : "user"}{" "}
-        {time ? time : "1 hour"} ago{" "}
-        <span className="pointer">
-          {commentsNumber ? commentsNumber : "00"} comments
+      <div className="news-article__data-container">
+        <span className="news-article__data-container--header">
+          {header ? header : "Placeholder text for article header"}
         </span>
+        <span className="news-article__data-container--link">
+          <a href={website} target="_blank">
+            {website ? formatUrl(website) : ""}
+          </a>
+        </span>
+
+        <div className="news-article__data-container--properties">
+          {points ? points : "00"} points by {user ? user : "user"}{" "}
+          {time ? time : "1 hour"} ago{" "}
+          <span className="pointer">
+            {commentsNumber ? commentsNumber : "00"} comments
+          </span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NewsArticle;
