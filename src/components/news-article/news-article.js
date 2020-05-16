0ec40e2c2195ec7050cdcad6ce14986625774875
MajaSveljo <majasveljo@gmail.com>
2020-05-16 09:58:30 +0200

    Added news-article components. Made a folder for homepage in pages. Added background for homepage.

---
+++ src/components/news-article/news-article.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./news-article.scss";
+
+const NewsArticle = () => (
+  <div className="news-article">
+    1.
+    <div>
+      <div className="news-article__heading">
+        Swift 5.3 Will Be Supported on Windows and Additional Linux
+        Distributions
+      </div>
+      <div className="news-article__link">(infoq.com)</div>
+      <div className="news-article__properties">
+        "251 points" by "nan0" "7 hours ago" "129 comments"
+      </div>
+    </div>
+  </div>
+);
+
+export default NewsArticle;
